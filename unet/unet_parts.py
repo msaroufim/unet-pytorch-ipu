ee3c79373bf727997f7b12584928b66efe33de79
Mark Saroufim <marksaroufim@gmail.com>
2020-11-30 21:16:18 +0000

    max group

--- unet/unet_parts.py
+++ unet/unet_parts.py
@@ -51,7 +51,7 @@ class Up(nn.Module):
             self.conv = DoubleConv(in_channels, out_channels, mid_channels=in_channels // 2, groups=64)
         else:
             self.up = nn.ConvTranspose2d(in_channels , in_channels // 2, groups= 64 , kernel_size=2, stride=2)
-            self.conv = DoubleConv(in_channels, out_channels, groups=64)
+            self.conv = DoubleConv(in_channels, out_channels, groups=out_channels)
 
 
     def forward(self, x1, x2):
